9c1d54c68476c1fb00601c7176ac08bfa4369256
Elsa Tutu <elsa.tuilliere@gmail.com>
2019-03-17 17:38:34 +0100

    exo_13

--- exo_12.rb
+++ exo_12.rb
@@ -3,4 +3,5 @@ i = gets.chomp.to_i
 
 i.times do |i|
 	puts "#{i}"
+	i = i + 1
 end
---
+++ exo_13.rb
@@ -0,0 +1,7 @@
+puts "Quelle est votre année de naissance ? "
+i = gets.chomp.to_i
+
+while i < 2018 do
+	puts i
+   i += 1
+end
