7fc3405a1d9160520ee796c8105f92d6921549bd
Elsa Tutu <elsa.tuilliere@gmail.com>
2019-03-18 11:19:59 +0100

    exo 20

--- pyramide.rb
+++ pyramide.rb
@@ -3,7 +3,7 @@ i = gets.chomp.to_i
 n = "#"
 f = 0
 
-until f > i # Jusqu’à ce que n soit plus grand que i, le code est exécuté.
+until f > i # Jusqu’à ce que n soit plus grand que 10, le code est exécuté.
   puts "#{n}#{n*f}"
   f = f + 1
 end   
